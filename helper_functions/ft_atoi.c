19caf9f263bbeb1e90f56e7ea1e601682512a563
Khalil Hafni <khafni@e1r8p8.1337.ma>
2021-02-12 11:57:57 +0100

    atoi handles overflow now

--- helper_functions/ft_atoi.c
+++ helper_functions/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: khafni <khafni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 23:11:59 by khafni            #+#    #+#             */
-/*   Updated: 2021/02/12 11:31:43 by khafni           ###   ########.fr       */
+/*   Updated: 2021/02/12 11:53:07 by khafni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 int		ft_atoi(const char *str)
 {
 	int				i;
-	unsigned long	nb;
+	long long		nb;
 	int				neg;
 
 	i = 0;
 	nb = 0;
 	neg = 1;
-	if (ft_strlen(str) > 8)
-		return (-1);
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
+	while (str[i] == '0')
+		i++;
+	if (ft_strlen(str + i) > 8)
+		return (-1);
 	if (str[i] == '-')
 		neg = -1;
 	if (str[i] == '-' || str[i] == '+')
@@ -32,6 +34,8 @@ int		ft_atoi(const char *str)
 	while ((str[i] >= '0') && (str[i] <= '9'))
 	{
 		nb = nb * 10 + str[i] - '0';
+		if (nb > 9223372036854775807)
+			return (neg < 0 ? 0 : -1);
 		i++;
 	}
 	return (neg * nb);
